1787136c66dc536be9850bb54f588d8d33029750
Hemudi <ksum1205@naver.com>
2023-06-18 06:05:13 +0900

    :sparkles: Add fonts data

---
+++ src/styles/theme/fonts.ts
@@ -0,0 +1,21 @@
+export type Fonts = typeof fonts;
+
+export const fonts = {
+  size: {
+    xxSmall: "1rem",
+    xSmall: "1.2rem",
+    small: "1.4rem",
+    base: "1.6rem",
+    medium: "1.8rem",
+    large: "2rem",
+    xLarge: "2.5rem",
+    xxLarge: "3rem",
+  },
+  weight: {
+    regular: 400,
+    bold: 700,
+  },
+  family: {
+    base: "'Noto Sans KR', sans-serif",
+  },
+};
