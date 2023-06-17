c64a9fd91ce05641d0e49aa59cbaf2e110526746
Hemudi <ksum1205@naver.com>
2023-06-18 05:32:25 +0900

    :sparkles: RootLayout metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import StyledComponentsRegistry from "@lib/styledComponentsRegistry";
 
+export const metadata = {
+  title: "해시태그 총공글 생성기 | HashTag Conggong Creator",
+  description:
+    "해시태그 총공글 생성기(HashTag Conggong Creator)는 트위터 실시간 트렌드 총공글을 쉽게 생성해주는 사이트입니다.",
+  keywords: ["해시태그 총공글 생성기", "HashTag Conggong Creator", "트위터", "해시총공", "실트총공"],
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
