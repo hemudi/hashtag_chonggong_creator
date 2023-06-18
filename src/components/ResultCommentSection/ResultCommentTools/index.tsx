49982fa0d718217998e33fe6440bee637bd7882d
Hemudi <ksum1205@naver.com>
2023-06-18 21:28:24 +0900

    :sparkles: Add ResultcommentTools component and stories

---
+++ src/components/ResultCommentSection/ResultCommentTools/index.tsx
@@ -0,0 +1,43 @@
+import Button from "@components/common/Button";
+import { useHashTagComment } from "@store/hashtag";
+import theme from "@styles/theme";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+const ResultCommentTools = () => {
+  const { hashTagComment } = useHashTagComment();
+  const [isSharable, setIsSharable] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (hashTagComment.length > 0) {
+      setIsSharable(() => true);
+    } else {
+      setIsSharable(() => false);
+    }
+  }, [hashTagComment]);
+
+  const handleCopyButtonClick = () => {};
+
+  const handleShareButtonClick = () => {};
+
+  return (
+    <S.ResultCommentTools>
+      <Button size="full" disabled={!isSharable} onClick={handleCopyButtonClick}>
+        복사하기
+      </Button>
+      <Button size="full" disabled={!isSharable} onClick={handleShareButtonClick}>
+        트위터로 공유하기
+      </Button>
+    </S.ResultCommentTools>
+  );
+};
+
+const S = {
+  ResultCommentTools: styled.div`
+    ${theme.mixins.flexBox()}
+    width: 100%;
+    gap: 10px;
+  `,
+};
+
+export default ResultCommentTools;
