0c4026bdc3b1156fa0ca2ba3248069499339a334
Hemudi <ksum1205@naver.com>
2023-06-18 12:56:06 +0900

    :sparkles: Add Header component and stories

---
+++ src/layout/Header/index.tsx
@@ -0,0 +1,31 @@
+import theme from "@styles/theme";
+import { styled } from "styled-components";
+
+const TITLE_TEXT = "#HashTag_Conggong_Creator";
+const DESCRIPTION_TEXT = "트위터 실시간 트렌드 총공글을 쉽게 생성해보세요!";
+
+const Header = () => {
+  return (
+    <S.Header>
+      <S.Title>{TITLE_TEXT}</S.Title>
+      <S.Description>{DESCRIPTION_TEXT}</S.Description>
+    </S.Header>
+  );
+};
+
+const S = {
+  Header: styled.header`
+    ${theme.mixins.flexBox("column")}
+    ${theme.mixins.preventDraggable()}
+    padding-bottom: 30px;
+  `,
+  Title: styled.h1`
+    font-size: ${theme.fonts.size.xxLarge};
+    font-weight: ${theme.fonts.weight.bold};
+  `,
+  Description: styled.span`
+    color: ${theme.palette.neutral_dark};
+  `,
+};
+
+export default Header;
