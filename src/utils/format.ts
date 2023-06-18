a72191571e7cda592eee13ad5c3acc43f4140362
Hemudi <ksum1205@naver.com>
2023-06-18 19:33:45 +0900

    :sparkles: Create parsedBlahComment function

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -4,3 +4,10 @@ export const createHashTag = (hashTagText: string): HashTag => {
   const hashTag = (hashTagText.indexOf("#") !== 0 ? "#" : "") + hashTagText.replaceAll(" ", "_");
   return hashTag as HashTag;
 };
+
+export const parsedBlahComment = (blahComment: string): string[] => {
+  return blahComment
+    .replace(/ /g, "") // 공백제거
+    .split("\n") // Enter 로 구분
+    .filter((value) => value !== "" && value.length >= 10); // 10자 넘는 문장만
+};
