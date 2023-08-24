eaee8ebbec6af1054897aacc0be7f36daae422aa
Hemudi <ksum1205@naver.com>
2023-08-24 13:48:12 +0900

    :lipstick: Style Layout component with tailwind

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,6 +1,3 @@
-import theme from "@styles/theme";
-import { styled } from "styled-components";
-
 const DEVELOPER_INFO = {
   NAME: "해삼스무디",
   LINK: "https://twitter.com/plave_smoothie",
@@ -8,30 +5,14 @@ const DEVELOPER_INFO = {
 
 const Footer = () => {
   return (
-    <S.Footer>
-      <S.Link href={DEVELOPER_INFO.LINK} target="_blank">{`Developed By. ${DEVELOPER_INFO.NAME}`}</S.Link>
-    </S.Footer>
+    <footer className="flex justify-center items-center w-full h-20 gap-2.5">
+      <a
+        className="text-gray text-xs hover:text-secondary-500"
+        href={DEVELOPER_INFO.LINK}
+        target="_blank"
+      >{`Developed By. ${DEVELOPER_INFO.NAME}`}</a>
+    </footer>
   );
-};
-
-const S = {
-  Footer: styled.footer`
-    ${theme.mixins.flexBox()}
-    width: 100%;
-    height: 60px;
-    gap: 10px;
-  `,
-  Author: styled.span`
-    color: ${theme.colors.grey700};
-    font-size: ${theme.fonts.size.small};
-  `,
-  Link: styled.a`
-    color: ${theme.colors.grey700};
-    font-size: ${theme.fonts.size.small};
-    &:hover {
-      color: ${theme.palette.primary_dark};
-    }
-  `,
 };
 
 export default Footer;
--- src/layout/Section/index.tsx
+++ src/layout/Section/index.tsx
@@ -1,47 +1,19 @@
-import theme from "@styles/theme";
 import { ReactNode } from "react";
-import { styled } from "styled-components";
 
 interface SectionProps {
   title: string;
   description?: ReactNode;
-  width?: `${number}px` | `${number}%` | "fit-content";
-  height?: `${number}px` | `${number}%` | "fit-content";
   children: ReactNode;
 }
 
-const Section = ({ title, description = "", width, height, children }: SectionProps) => {
+const Section = ({ title, description = "", children }: SectionProps) => {
   return (
-    <S.Section width={width} height={height}>
-      <S.Title>{title}</S.Title>
-      <S.Description>{description}</S.Description>
+    <section className="flex flex-col items-start justify-center p-2.5 w-full h-fit">
+      <h2 className="w-full text-lg">{title}</h2>
+      <p className="w-full text-sm text-gray whitespace-pre-line leading-5 pt-1 pb-2.5">{description}</p>
       {children}
-    </S.Section>
+    </section>
   );
-};
-
-const S = {
-  Section: styled.section<Pick<SectionProps, "width" | "height">>`
-    ${theme.mixins.flexBox("column", "flex-start")}
-    width: ${({ width = "100%" }) => width};
-    height: ${({ height = "100%" }) => height};
-    width: 100%;
-    min-height: fit-content;
-    padding: 10px;
-  `,
-  Title: styled.h2`
-    width: 100%;
-    font-size: ${theme.fonts.size.medium};
-    font-weight: ${theme.fonts.weight.bold};
-  `,
-  Description: styled.p`
-    width: 100%;
-    white-space: pre-line;
-    font-size: ${theme.fonts.size.small};
-    line-height: ${theme.fonts.size.medium};
-    color: ${theme.colors.grey900};
-    padding: 5px 0 10px 0;
-  `,
 };
 
 export default Section;
