b2e265ce780077734fefa9d3e3edf31aefd0eb3c
Hemudi <ksum1205@naver.com>
2023-09-12 17:22:39 +0900

    :sparkles: GA 이벤트 함수 구현 및 적용

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,14 +4,11 @@ import { Analytics } from "@vercel/analytics/react";
 import HashTagInputSection from "@components/HashTagInputSection";
 import BlahCommentSection from "@components/BlahCommentSection";
 import ResultCommentSection from "@components/ResultCommentSection";
-import { useEffect } from "react";
-import * as gtag from "@lib/gtag";
-import { useRouter } from "next/navigation";
-import { useNavigationEvent } from "@hooks/useNavigationEvent";
+import { pageView } from "@lib/gtag";
+import useNavigationEvent from "@hooks/useNavigationEvent";
 
 export default function HomePage() {
-  const router = useRouter();
-
+  useNavigationEvent(pageView);
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-white rounded-md p-2.5 h-fit">
--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,10 +1,12 @@
 export const GOOGLE_ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url: URL) => {
-  window.gtag("config", GOOGLE_ANALYTICS as string, {
-    page_path: url,
-  });
+export const pageView = (url: URL | string) => {
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("config", GOOGLE_ANALYTICS as string, {
+      page_path: url,
+    });
+  }
 };
 
 type GTagEvent = {
@@ -16,9 +18,11 @@ type GTagEvent = {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if (typeof window.gtag !== "undefined") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };
