c50c543e3f141311e9a36401821fb5beae8eb7b3
Hemudi <ksum1205@naver.com>
2023-10-30 23:14:23 +0900

    :bug: 배경 이미지가 화면 가득차게 수정

--- src/layout/Layout/index.tsx
+++ src/layout/Layout/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col justify-start items-center min-h-screen">
+    <div className="relative flex flex-col justify-start items-center min-h-screen">
       <Image className="-z-10 object-cover" src="/assets/images/background.jpg" alt="background image" fill />
       <Header />
       <Main>{children}</Main>
